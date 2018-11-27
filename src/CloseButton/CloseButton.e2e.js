02104a171e3ae2289a0180568a15291a4fd5397b
mykas <mykolass@wix.com>
2018-11-27 11:39:31 +0200

    Adding e2e tests

---
+++ src/CloseButton/CloseButton.e2e.js
@@ -0,0 +1,36 @@
+import eyes from 'eyes.it'
+import autoExampleDriver from 'wix-storybook-utils/AutoExampleDriver'
+
+import { createStoryUrl } from '../../test/utils/storybook-helpers'
+import { storySettings } from '../../stories/CloseButton/storySettings'
+
+describe('CloseButton', () => {
+  const storyUrl = createStoryUrl({
+    kind: storySettings.kind,
+    story: storySettings.storyName
+  })
+
+  beforeAll(async () => {
+    await browser.get(storyUrl)
+  })
+
+  beforeEach(() => {
+    return autoExampleDriver.remount()
+  })
+
+  describe(`'skin' prop`, () => {
+    ;[' ', 'inverted', 'light', 'dark', 'transparent'].map(skin =>
+      eyes.it(`should render with value ${skin}`, async () => {
+        await autoExampleDriver.setProps({ skin })
+      })
+    )
+  })
+
+  describe(`'size' prop`, () => {
+    ;['small', ' '].map(size =>
+      eyes.it(`should render with ${size} icon`, async () => {
+        await autoExampleDriver.setProps({ size })
+      })
+    )
+  })
+})
