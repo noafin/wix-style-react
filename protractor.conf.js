91b660ad02151757685270a6c725cfb784fc0b21
mykas <mykolass@wix.com>
2018-11-27 17:43:13 +0200

    Enable headles mode

--- protractor.conf.js
+++ protractor.conf.js
@@ -2,6 +2,18 @@ module.exports.config = {
   specs: ['test/**/*.e2e.js', 'src/**/*.e2e.js'],
   baseUrl: `http://localhost:6006/`,
   jasmineNodeOpts: { defaultTimeoutInterval: 60000 },
+  capabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+      args: [
+        'ignore-certificate-errors',
+        `proxy-server=127.0.0.1:3333`,
+        'disable-extensions',
+        'disable-plugins',
+        '--headless',
+      ],
+    },
+  },
   onPrepare() {
     browser.ignoreSynchronization = true;
   },
